1c119b7bb1975818ac74f34323b67315591f1267
Sabrina Lewis <sabrina.lewis@bryanuniversity.edu>
2020-11-16 17:18:51 -0600

    finalized calculator

--- src/Components/Calculator.js
+++ src/Components/Calculator.js
@@ -1,5 +1,6 @@
 import React, {useState} from 'react';
 import CalculatorTitle from './CalculatorTitle';
+import Footer from './Footer';
 
 function Calculator() {
   const [questionDisplay, setQuestionDisplay] = useState("")
@@ -36,8 +37,11 @@ function Calculator() {
 
     return (
       <div className="frame">
+
         <CalculatorTitle value="React Calculator"/>
+
         <div className="mainCalc">
+
           <div className="screen">
               <div className="screenRow">
                   <input id="questionDisplay" name="question" type="text" value={questionDisplay} readOnly/>
@@ -45,35 +49,38 @@ function Calculator() {
                   <input id="answerDisplay" name="answer" type="text" value={answerDisplay} readOnly/>
               </div>
           </div>
-          <div className="buttonRow">
-            <button value={"Clear"} onClick={clearScreen}>Clear</button>
-            <button value={"Delete"} onClick={deleteCharacter}>Delete</button>
-            <button value={"."} onClick={displayNumbers}>.</button>
-            <button value={"/"} onClick={event => displayNumbers(event)}>/</button>
+
+          <div className="buttonsContainer">
+              <button value={"Delete"} onClick={deleteCharacter}>Delete</button>
+              <button value={"/"} onClick={event => displayNumbers(event)}>/</button>
+              <button value={"*"} onClick={displayNumbers}>*</button>
+              <button value={"-"} onClick={displayNumbers}>-</button>
+
+              <button value={"7"} onClick={event => displayNumbers(event)}>7</button>
+              <button value={"8"} onClick={event => displayNumbers(event)}>8</button>
+              <button value={"9"} onClick={event => displayNumbers(event)}>9</button>
+              <button value={"+"} className="no-bottom-border" onClick={displayNumbers}>+</button>
+
+              <button value={"4"} onClick={displayNumbers}>4</button>
+              <button value={"5"} onClick={displayNumbers}>5</button>
+              <button value={"6"} onClick={displayNumbers}>6</button>
+              <button value={"+"} className="no-top-border" onClick={displayNumbers}></button>
+
+              <button value={"1"} onClick={displayNumbers}>1</button>
+              <button value={"2"} onClick={displayNumbers}>2</button>
+              <button value={"3"} onClick={displayNumbers}>3</button>
+              <button value={"="} className="no-bottom-border" onClick={calculateAnswer}>=</button>
+
+              <button value={"Clear"} onClick={clearScreen}>Clear</button>
+              <button value={"0"} onClick={displayNumbers}>0</button>
+              <button value={"."} onClick={displayNumbers}>.</button>
+              <button value={""} className="no-top-border" onClick={calculateAnswer}></button>
           </div>
-          <div className="buttonRow">
-            <button value={"7"} onClick={event => displayNumbers(event)}>7</button>
-            <button value={"8"} onClick={event => displayNumbers(event)}>8</button>
-            <button value={"9"} onClick={event => displayNumbers(event)}>9</button>
-            <button value={"*"} onClick={displayNumbers}>*</button>
-          </div>
-          <div className="buttonRow">
-            <button value={"4"} onClick={displayNumbers}>4</button>
-            <button value={"5"} onClick={displayNumbers}>5</button>
-            <button value={"6"} onClick={displayNumbers}>6</button>
-            <button value={"-"} onClick={displayNumbers}>-</button>
-          </div>
-          <div className="buttonRow">
-            <button value={"1"} onClick={displayNumbers}>1</button>
-            <button value={"2"} onClick={displayNumbers}>2</button>
-            <button value={"3"} onClick={displayNumbers}>3</button>
-            <button value={"+"} onClick={displayNumbers}>+</button>
-          </div>
-          <div className="buttonRow">
-            <button value={"0"} onClick={displayNumbers}>0</button>
-            <button value={"="} onClick={calculateAnswer}>=</button>
-          </div>
+
         </div>
+     
+        <Footer />
+
       </div>
     )
   }
---
+++ src/Components/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Footer() {
+
+    return (
+        <div>
+            <a href="http://www.sabrinalewis.me">
+                <h1 className="copyright">(c) 2020  Sabrina  Lewis</h1></a>
+        </div>
+    )
+}
+
+export default Footer;
