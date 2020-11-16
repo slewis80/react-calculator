a8e6c2e1f59bb9a0b26ed8959f016b1665b698fe
Sabrina Lewis <sabrina.lewis@bryanuniversity.edu>
2020-11-16 17:18:15 -0600

    Updates

---
+++ precache-manifest.018ed7aed313f44bd3e133b7560d0d8a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8fb71154f11a3b69fdc032d917b4de15",
+    "url": "/react-calculator/index.html"
+  },
+  {
+    "revision": "c688890ef17e8a870450",
+    "url": "/react-calculator/static/css/main.d2da2c87.chunk.css"
+  },
+  {
+    "revision": "e7332a5ef994a7079aa4",
+    "url": "/react-calculator/static/js/2.d8c5a45e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-calculator/static/js/2.d8c5a45e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c688890ef17e8a870450",
+    "url": "/react-calculator/static/js/main.1a1a0ae3.chunk.js"
+  },
+  {
+    "revision": "7a4e8bb32f6cfb799366",
+    "url": "/react-calculator/static/js/runtime-main.6252dc75.js"
+  }
+]);
